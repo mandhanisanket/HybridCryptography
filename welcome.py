cfc49e4e458ce3943da227bc723b9f853f30c276
manali2306 <jmanali27@gmail.com>
2018-04-10 22:06:49 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -7,17 +7,38 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from upload import Ui_Dialog2
+from download import Ui_Dialog3
 
 class Ui_Dialog1(object):
+    def openWindow1(self):
+        self.window1 = QtWidgets.QDialog()
+        self.ui = Ui_Dialog2()
+        self.ui.setupUi(self.window1)
+        Dialog1.hide()
+        self.window1.show()
+
+    def openWindow2(self):
+        self.window2 = QtWidgets.QDialog()
+        self.ui = Ui_Dialog3()
+        self.ui.setupUi(self.window2)
+        Dialog1.hide()
+        self.window2.show()
+  
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(451, 337)
         self.pushButton = QtWidgets.QPushButton(Dialog)
         self.pushButton.setGeometry(QtCore.QRect(200, 100, 75, 23))
         self.pushButton.setObjectName("pushButton")
+
+        self.pushButton.clicked.connect(self.openWindow1)
+        
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(200, 170, 75, 23))
         self.pushButton_2.setObjectName("pushButton_2")
+
+        self.pushButton_2.clicked.connect(self.openWindow2)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -32,9 +53,9 @@ class Ui_Dialog1(object):
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    Dialog1 = QtWidgets.QDialog()
+    ui = Ui_Dialog1()
+    ui.setupUi(Dialog1)
+    Dialog1.show()
     sys.exit(app.exec_())
 
