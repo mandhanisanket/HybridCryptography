a82007848eb73d1be53cfdf2484443236819b37c
manali2306 <jmanali27@gmail.com>
2018-04-12 23:45:58 +0530

    Update welcome.py
    pyqt4

--- welcome.py
+++ welcome.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'upload1.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
 from PyQt4 import QtCore, QtGui
 from upload1 import Ui_Dialog2
 from download1 import Ui_Dialog3
