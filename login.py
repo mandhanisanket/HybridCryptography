5364e0c8492aad3e0486e8c4e34f122839f05a93
mandhanisanket <mandhaniasanket@gmail.com>
2018-04-12 20:39:55 +0530

    Update login.py
    fixed  multiple errors

--- login.py
+++ login.py
@@ -1,89 +1,97 @@
-# -*- coding: utf-8 -*-
+from PyQt4 import QtCore, QtGui
+from welcome1 import Ui_welcomed
+import _mysql
 
-# Form implementation generated from reading ui file 'login.ui'
-#
-# Created by: PyQt5 UI code generator 5.6
-#
-# WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from welcome import Ui_Dialog1
-import sqlite3
-class Ui_Dialog(object):
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Login(object):
     def openWindow(self):
-        self.window = QtWidgets.QDialog()
-        self.ui = Ui_Dialog1()
+        self.window = QtGui.QDialog()
+        self.ui = Ui_welcomed()
         self.ui.setupUi(self.window)
-        Dialog.hide()
+        Login.hide()
         self.window.show()
+        
     def loginCheck(self):
-         username = self.uname_edit.text()
+         username = self.uname_linedit.text()
          password = self.pass_edit.text()
-
-         connection = sqlite3.connect("login.db")
-         result = connection.execute("SELECT * FROM USERS WHERE USERNAME = ?  AND PASSWORD = ?",(username,password))
-         if(len(result.fetchall()) > 0):
+	 connection = _mysql.connect('localhost', 'root', '1234', 'login')
+         connection.query("SELECT * FROM USERS;")
+         result=connection.use_result()
+    	 #print(result.fetch_row())
+    	 
+         if(len(result.fetch_row()) > 0):
              print("User Found")
              self.openWindow()
          else:
              print("User Not Found")
-         
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(450, 336)
-        Dialog.setStyleSheet("background-color:rgb(0, 170, 255)qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255))")
-        self.login_btn = QtWidgets.QPushButton(Dialog)
-        self.login_btn.setGeometry(QtCore.QRect(180, 190, 75, 23))
-        self.login_btn.setObjectName("login_btn")
-        ####Button event####
-        self.login_btn.clicked.connect(self.loginCheck)  
-        self.uname_edit = QtWidgets.QLineEdit(Dialog)
-        self.uname_edit.setGeometry(QtCore.QRect(210, 120, 113, 20))
-        self.uname_edit.setObjectName("uname_edit")
-        self.pass_edit = QtWidgets.QLineEdit(Dialog)
-        self.pass_edit.setGeometry(QtCore.QRect(210, 150, 113, 20))
-        self.pass_edit.setObjectName("pass_edit")
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(120, 120, 71, 16))
+    def setupUi(self, Login):
+        Login.setObjectName(_fromUtf8("Login"))
+        Login.resize(476, 347)
+        self.label = QtGui.QLabel(Login)
+        self.label.setGeometry(QtCore.QRect(110, 130, 71, 16))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label.setFont(font)
-        self.label.setAlignment(QtCore.Qt.AlignBottom|QtCore.Qt.AlignHCenter)
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(120, 150, 71, 16))
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(Login)
+        self.label_2.setGeometry(QtCore.QRect(110, 170, 71, 16))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label_2.setFont(font)
-        self.label_2.setAlignment(QtCore.Qt.AlignBottom|QtCore.Qt.AlignHCenter)
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(150, 70, 141, 31))
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.uname_linedit = QtGui.QLineEdit(Login)
+        self.uname_linedit.setGeometry(QtCore.QRect(210, 130, 113, 20))
+        self.uname_linedit.setObjectName(_fromUtf8("uname_linedit"))
+        self.pass_edit = QtGui.QLineEdit(Login)
+        self.pass_edit.setGeometry(QtCore.QRect(210, 170, 113, 20))
+        self.pass_edit.setObjectName(_fromUtf8("pass_edit"))
+        
+        self.pass_edit.setEchoMode(QtGui.QLineEdit.Password)
+        self.login_btn = QtGui.QPushButton(Login)
+        self.login_btn.setGeometry(QtCore.QRect(180, 210, 75, 23))
+        self.login_btn.setObjectName(_fromUtf8("login_btn"))
+        self.login_btn.clicked.connect(self.loginCheck)
+        self.label_3 = QtGui.QLabel(Login)
+        self.label_3.setGeometry(QtCore.QRect(130, 70, 161, 31))
         font = QtGui.QFont()
-        font.setFamily("MV Boli")
-        font.setPointSize(16)
+        font.setFamily(_fromUtf8("MV Boli"))
+        font.setPointSize(18)
         self.label_3.setFont(font)
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_3.setObjectName("label_3")
+        self.label_3.setObjectName(_fromUtf8("label_3"))
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(Login)
+        QtCore.QMetaObject.connectSlotsByName(Login)
 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.login_btn.setText(_translate("Dialog", "Login"))
-        self.label.setText(_translate("Dialog", "USERNAME"))
-        self.label_2.setText(_translate("Dialog", "PASSWORD"))
-        self.label_3.setText(_translate("Dialog", "LOGIN PAGE"))
+    def retranslateUi(self, Login):
+        Login.setWindowTitle(_translate("Login", "Dialog", None))
+        self.label.setText(_translate("Login", "USERNAME", None))
+        self.label_2.setText(_translate("Login", "PASSWORD", None))
+        self.login_btn.setText(_translate("Login", "Login", None))
+        self.label_3.setText(_translate("Login", "LOGIN PAGE", None))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    app = QtGui.QApplication(sys.argv)
+    Login = QtGui.QDialog()
+    ui = Ui_Login()
+    ui.setupUi(Login)
+    Login.show()
     sys.exit(app.exec_())
-
