40f3539c8b2d59905d79d4071ad8816c69d9557f
mandhanisanket <mandhaniasanket@gmail.com>
2018-04-12 20:41:13 +0530

    Update welcome.py
    pyqt4 version of code

--- welcome.py
+++ welcome.py
@@ -1,61 +1,72 @@
-# -*- coding: utf-8 -*-
+from PyQt4 import QtCore, QtGui
+from upload1 import Ui_Dialog2
+from download1 import Ui_Dialog3
 
-# Form implementation generated from reading ui file 'welcome.ui'
-#
-# Created by: PyQt5 UI code generator 5.6
-#
-# WARNING! All changes made in this file will be lost!
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from upload import Ui_Dialog2
-from download import Ui_Dialog3
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_Dialog1(object):
+class Ui_welcomed(object):
     def openWindow1(self):
-        self.window1 = QtWidgets.QDialog()
+        self.window1 = QtGui.QDialog()
         self.ui = Ui_Dialog2()
         self.ui.setupUi(self.window1)
-        Dialog1.hide()
+        #welcomed.hide()
         self.window1.show()
 
     def openWindow2(self):
-        self.window2 = QtWidgets.QDialog()
+        self.window2 = QtGui.QDialog()
         self.ui = Ui_Dialog3()
         self.ui.setupUi(self.window2)
-        Dialog1.hide()
+        #welcomed.hide()
         self.window2.show()
-  
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(451, 337)
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(200, 100, 75, 23))
-        self.pushButton.setObjectName("pushButton")
-
-        self.pushButton.clicked.connect(self.openWindow1)
         
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(200, 170, 75, 23))
-        self.pushButton_2.setObjectName("pushButton_2")
-
-        self.pushButton_2.clicked.connect(self.openWindow2)
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(473, 348)
+        self.upload_btn = QtGui.QPushButton(Dialog)
+        self.upload_btn.setGeometry(QtCore.QRect(175, 120, 110, 23))
+        self.upload_btn.setObjectName(_fromUtf8("upload_btn"))
+        self.upload_btn.clicked.connect(self.openWindow1)
+        
+        self.download_btn = QtGui.QPushButton(Dialog)
+        self.download_btn.setGeometry(QtCore.QRect(175, 170, 110, 23))
+        self.download_btn.setObjectName(_fromUtf8("download_btn"))
+        self.download_btn.clicked.connect(self.openWindow2)
+        
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(150, 60, 161, 41))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName(_fromUtf8("label"))
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "Upload file "))
-        self.pushButton_2.setText(_translate("Dialog", "Download file"))
+        Dialog.setWindowTitle(_translate("Dialog", "Choose a task", None))
+        self.upload_btn.setText(_translate("Dialog", "Upload File", None))
+        self.download_btn.setText(_translate("Dialog", "Download File", None))
+        self.label.setText(_translate("Dialog", "Choose a task ", None))
 
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog1 = QtWidgets.QDialog()
-    ui = Ui_Dialog1()
-    ui.setupUi(Dialog1)
-    Dialog1.show()
+    app = QtGui.QApplication(sys.argv)
+    welcomed = QtGui.QDialog()
+    ui = Ui_welcomed()
+    ui.setupUi(welcomed)
+    welcomed.show()
     sys.exit(app.exec_())
-
